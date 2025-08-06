96ea91b5f8ae975a23e59e6201eeb86a84ac84a8
manishanimje <nimjemanisha2@gmail.com>
2025-08-06 10:58:54 +0530

    Fix: some push

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,35 +1,7 @@
-export default async function BlogPage() {
-  try {
-    const res = await fetch(`http://localhost:4000/api/posts`, {
-      cache: "no-store",
-    });
+import React from "react";
 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch posts. Status: ${res.status}`);
-    }
+const page = () => {
+  return <div>page</div>;
+};
 
-    const posts = await res.json();
-
-    return (
-      <main className="p-8">
-        <h1 className="text-3xl font-bold">Blog Posts</h1>
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id} className="mt-4">
-              <h2 className="text-xl">{post.title}</h2>
-              <p>{post.content}</p>
-            </li>
-          ))}
-        </ul>
-      </main>
-    );
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return (
-      <main className="p-8">
-        <h1 className="text-3xl font-bold">Blog Posts</h1>
-        <p className="text-red-500 mt-4">Failed to load posts.</p>
-      </main>
-    );
-  }
-}
+export default page;
--- src/app/imports/Header/Headernew.js
+++ src/app/imports/Header/Headernew.js
@@ -1,6 +1,10 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Headernew = () => {
+  const router = useRouter();
+
   return (
     <>
       <header className="header-h-four">
@@ -19,44 +23,66 @@ const Headernew = () => {
                 <div className="main-menu main-menu-4 text-center">
                   <nav id="mobile-menu-four">
                     <ul>
-                      <li className="has-dropdown">
-                        <a href="/">Home</a>
+                      <li
+                        className="has-dropdown"
+                        onClick={() => router.push("/")}
+                      >
+                        <span>Home</span>
                       </li>
-                      <li>
-                        <a href="about">About</a>
+                      <li onClick={() => router.push("/about")}>
+                        <span>About</span>
                       </li>
-                      <li className="mega-menu">
-                        <a href="shop">Shop</a>
+                      <li
+                        className="mega-menu"
+                        onClick={() => router.push("/shop")}
+                      >
+                        <span>Shop</span>
                       </li>
 
                       <li>
-                        <a href="#">Pages</a>
+                        <span onClick={() => router.push("#")}>Pages</span>
                         <ul className="submenu">
                           <li>
-                            <a href="about.html">About Us</a>
+                            <span onClick={() => router.push("/about")}>
+                              About Us
+                            </span>
                           </li>
                           <li>
-                            <a href="contact.html">Contact Us</a>
+                            <span onClick={() => router.push("/contact")}>
+                              Contact Us
+                            </span>
                           </li>
                           <li>
-                            <a href="login.html">login</a>
+                            <span onClick={() => router.push("/login")}>
+                              Login
+                            </span>
                           </li>
                           <li>
-                            <a href="register.html">Register</a>
+                            <span onClick={() => router.push("/register")}>
+                              Register
+                            </span>
                           </li>
                           <li>
-                            <a href="cart.html">Shoping Cart</a>
+                            <span onClick={() => router.push("/cart")}>
+                              Shopping Cart
+                            </span>
                           </li>
                           <li>
-                            <a href="checkout.html">Checkout</a>
+                            <span onClick={() => router.push("/checkout")}>
+                              Checkout
+                            </span>
                           </li>
                           <li>
-                            <a href="wishlist.html">Wishlist</a>
+                            <span onClick={() => router.push("/wishlist")}>
+                              Wishlist
+                            </span>
                           </li>
                         </ul>
                       </li>
                       <li>
-                        <a href="contact.html">Contact</a>
+                        <span onClick={() => router.push("/contact")}>
+                          Contact
+                        </span>
                       </li>
                     </ul>
                   </nav>
@@ -64,18 +90,31 @@ const Headernew = () => {
               </div>
               <div className="col-xl-2 col-lg-2 col-md-7 col-6">
                 <div className="header-left-icon header-left-icon4 d-flex align-items-center f-right">
-                  <a className="search-btn nav-search search-trigger" href="#">
+                  <span
+                    className="search-btn nav-search search-trigger"
+                    onClick={() => router.push("#")}
+                    style={{ cursor: "pointer" }}
+                  >
                     <i className="fa-solid fa-search" />
-                  </a>
-                  <a href="login.html">
+                  </span>
+                  <span
+                    onClick={() => router.push("/login")}
+                    style={{ cursor: "pointer" }}
+                  >
                     <i className="fa-solid fa-user" />
-                  </a>
-                  <a href="wishlist.html">
+                  </span>
+                  <span
+                    onClick={() => router.push("/wishlist")}
+                    style={{ cursor: "pointer" }}
+                  >
                     <i className="fa-solid fa-heart" />
-                  </a>
-                  <a href="cart.html">
+                  </span>
+                  <span
+                    onClick={() => router.push("/cart")}
+                    style={{ cursor: "pointer" }}
+                  >
                     <i className="fa-solid fa-cart-arrow-down" />
-                  </a>
+                  </span>
                 </div>
               </div>
               <div className="col-2 col-md-1 d-block d-lg-none">
@@ -102,137 +141,14 @@ const Headernew = () => {
           <ul id="mobile-menu-active">
             <li>
               <a href="index.html">Home</a>
-              <ul className="submenu submenu-three">
-                <li>
-                  <a href="index.html">Home Style 1</a>
-                </li>
-                <li>
-                  <a href="index-2.html">Home Style 2</a>
-                </li>
-                <li>
-                  <a href="index-3.html">Home Style 3</a>
-                </li>
-                <li>
-                  <a href="index-4.html">Home Style 4</a>
-                </li>
-                <li>
-                  <a href="index-5.html">Home Style 5</a>
-                </li>
-              </ul>
             </li>
             <li>
               <a href="about.html">About</a>
             </li>
             <li className="has-dropdown">
               <a href="shop.html">Shop</a>
-              <ul className="submenu">
-                <li className="has-dropdown">
-                  <a href="#">Category View</a>
-                  <ul className="submenu">
-                    <li>
-                      <a href="shop-2-col.html">Shop 2 Column</a>
-                    </li>
-                    <li>
-                      <a href="shop-2-col-right-sidebar.html">
-                        Shop 2 Right Sidebar
-                      </a>
-                    </li>
-                    <li>
-                      <a href="shop-4-col.html">Shop 4 Column Full</a>
-                    </li>
-                    <li>
-                      <a href="shop-list-view.html">Shop List View</a>
-                    </li>
-                    <li>
-                      <a href="shop-no-sidebar.html">Shop No Sidebar</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="has-dropdown">
-                  <a href="#">Category View</a>
-                  <ul className="submenu">
-                    <li>
-                      <a href="shop.html">Sidebar Left</a>
-                    </li>
-                    <li>
-                      <a href="shop-right-sidebar.html">Sidebar Right</a>
-                    </li>
-                    <li>
-                      <a href="shop-4-col.html">Style 01</a>
-                    </li>
-                    <li>
-                      <a href="shop-2-col-right-sidebar.html">Style 02</a>
-                    </li>
-                    <li>
-                      <a href="shop-2-col.html">Style 03</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="has-dropdown">
-                  <a href="#">Products Types</a>
-                  <ul className="submenu">
-                    <li>
-                      <a href="product-details.html">Simple Product</a>
-                    </li>
-                    <li>
-                      <a href="product-details.html">Variable Product</a>
-                    </li>
-                    <li>
-                      <a href="product-details-grouped.html">Product Grouped</a>
-                    </li>
-                    <li>
-                      <a href="product-details-right.html">Thumb Right Side</a>
-                    </li>
-                    <li>
-                      <a href="product-details-2.html">Thumb Bottom Side</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
             </li>
-            <li className="has-dropdown">
-              <a href="blog.html">News</a>
-              <ul className="submenu">
-                <li>
-                  <a href="blog.html">Blog Standart</a>
-                </li>
-                <li>
-                  <a href="blog-left-sidebar.html">Blog Left Sidebar</a>
-                </li>
-                <li>
-                  <a href="blog-no-sidebar.html">Blog No Sidebar</a>
-                </li>
-                <li>
-                  <a href="blog-2-col.html">Blog 2 Column</a>
-                </li>
-                <li>
-                  <a href="blog-2-col-mas.html">Blog 2 Col Masonry</a>
-                </li>
-                <li>
-                  <a href="blog-3-col.html">Blog 3 Column</a>
-                </li>
-                <li>
-                  <a href="blog-3-col-mas.html">Blog 3 Col Masonry</a>
-                </li>
-                <li>
-                  <a href="blog-details.html">Blog Details</a>
-                </li>
-                <li>
-                  <a href="blog-details-audio.html">Blog Details Audio</a>
-                </li>
-                <li>
-                  <a href="blog-details-video.html">Blog Details Video</a>
-                </li>
-                <li>
-                  <a href="blog-details-gallery.html">Blog Details Gallery</a>
-                </li>
-                <li>
-                  <a href="blog-details-left-sidebar.html">
-                    Details Left Sidebar
-                  </a>
-                </li>
-              </ul>
-            </li>
+
             <li className="has-dropdown">
               <a href="#">Pages</a>
               <ul className="submenu">
